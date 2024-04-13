d3750b142785d80c0c010e71fcd0950fa04b5e6e
Andresfetobon <andrestromp777@gmail.com>
2024-04-12 23:44:58 -0500

    actualizacion

--- app/add-new-category-item.jsx
+++ app/add-new-category-item.jsx
@@ -5,77 +5,116 @@ import {
   StyleSheet,
   TextInput,
   TouchableOpacity,
+  ScrollView,
+  KeyboardAvoidingView,
 } from 'react-native';
 import React, { useState } from 'react';
 import Colors from '../utils/Colors';
 import { Ionicons } from '@expo/vector-icons';
 import { FontAwesome5 } from '@expo/vector-icons';
 import * as ImagePicker from 'expo-image-picker';
+import { decode } from 'base64-arraybuffer'
+import { supabase } from '../utils/SupaBaseConfig'
 const placeholder =
   'https://storage.googleapis.com/proudcity/mebanenc/uploads/2021/03/placeholder-image.png';
 
 export default function addNewCategoryItem() {
   const [image, setImage] = useState(placeholder);
-  const [PreviewImage, setPreviewImage] = useState()
+  const [previewImage, setPreviewImage] = useState(placeholder);
+  const [name, setName] = useState();
+  const [url, setUrl] = useState();
+  const [cost, setCost] = useState();
+  const [note, setNote] = useState();
 
   const onImagePick = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: false,
-      quality: 1,
-      base64: true
+      quality: 0.7,
+      base64: true,
     });
 
-    
     if (!result.canceled) {
       setPreviewImage(result.assets[0].uri);
       setImage(result.assets[0].base64);
     }
   };
 
+  const onClickAdd = async () => {
+    const filName = Date.now();
+    const { data, error } = await supabase.storage
+      .from('images')
+      .upload(filName + '.png', decode(image), {
+        contentType: 'image/png',
+      });
+      if(data){
+        const fileUrl = 'https://iekizkkusjfsusgezwyq.supabase.co/storage/v1/object/public/images/' + data.path;
+        console.log(fileUrl)
+      }
+
+  };
+
   return (
-    <View style={{ padding: 20 }}>
-      <TouchableOpacity onPress={() => onImagePick()}>
-        <Image source={{ uri: PreviewImage }} style={styles.image} />
-      </TouchableOpacity>
-      <View style={styles.textInputContainer}>
-        <Ionicons name='pricetag' size={24} color={Colors.GRAY} />
-        <TextInput placeholder='Item Name' styles={styles.inputText} />
-      </View>
-      <View style={styles.textInputContainer}>
-        <FontAwesome5 name='dollar-sign' size={24} color={Colors.GRAY} />
-        <TextInput placeholder='Cost' styles={styles.inputText} />
-      </View>
-      <View style={styles.textInputContainer}>
-        <Ionicons name='link' size={24} color={Colors.GRAY} />
-        <TextInput placeholder='Url' styles={styles.inputText} />
-      </View>
-      <View style={styles.textInputContainer}>
-        <Ionicons name='pencil' size={24} color={Colors.GRAY} />
-        <TextInput
-          placeholder='Note'
-          styles={styles.inputText}
-          numberOfLines={3}
-        />
-      </View>
-      <TouchableOpacity style={styles.button}>
-        <Text
-          style={{
-            textAlign: 'center',
-            color: Colors.WHITE,
-            fontSize: 17,
-            fontFamily: 'outfit-bold',
-          }}
-        >
-          Add
-        </Text>
-      </TouchableOpacity>
-    </View>
+    <KeyboardAvoidingView>
+      <ScrollView style={{ padding: 20 }}>
+        <TouchableOpacity onPress={() => onImagePick()}>
+          <Image source={{ uri: previewImage }} style={styles.imageContainer} />
+        </TouchableOpacity>
+        <View style={styles.textInputContainer}>
+          <Ionicons name='pricetag' size={24} color={Colors.GRAY} />
+          <TextInput
+            style={styles.input}
+            placeholder='Item Name'
+            onChangeText={value => setName(value)}
+          />
+        </View>
+        <View style={styles.textInputContainer}>
+          <FontAwesome5 name='dollar-sign' size={24} color={Colors.GRAY} />
+          <TextInput
+            style={styles.input}
+            keyboardType='number-pad'
+            onChangeText={value => setCost(value)}
+            placeholder='Cost'
+          />
+        </View>
+        <View style={styles.textInputContainer}>
+          <Ionicons name='link' size={24} color={Colors.GRAY} />
+          <TextInput
+            style={styles.input}
+            placeholder='Url'
+            onChangeText={value => setUrl(value)}
+          />
+        </View>
+        <View style={styles.textInputContainer}>
+          <Ionicons name='pencil' size={24} color={Colors.GRAY} />
+          <TextInput
+            style={styles.input}
+            placeholder='Note'
+            onChangeText={value => setNote(value)}
+            numberOfLines={3}
+          />
+        </View>
+        <TouchableOpacity 
+        onPress={() => onClickAdd()}
+        disabled={!name || !cost} style={styles.button}>
+          <Text
+            style={{
+              textAlign: 'center',
+              color: Colors.WHITE,
+              fontSize: 17,
+              fontFamily: 'outfit-bold',
+            }}
+          >
+            Add
+          </Text>
+        </TouchableOpacity>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
-  image: {
+  imageContainer: {
     width: 150,
     height: 150,
     backgroundColor: Colors.GRAY,
@@ -92,8 +131,9 @@ const styles = StyleSheet.create({
     borderColor: Colors.GRAY,
     marginTop: 10,
   },
-  inputText: {
+  input: {
     fontSize: 17,
+    width: '100%',
   },
   button: {
     padding: 17,
